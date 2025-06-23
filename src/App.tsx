82e9372f3b982f74746638fe61e801c6cc615af4
berith <berith.py@gmail.com>
2025-06-22 23:08:21 -0300

    Add gtag, add icon, fix margins

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
 
   // State for current card items, initialized with one example card
   const [cardItems, setCardItems] = useState<{ type: ItemType; label: string }[]>(exampleCardItems)
+
   // Generates a random card based on enabled word sources
   const generateRandomCard = () => {
     const enabled = allowedOptions.filter((ws) => ws.enabled)
@@ -30,26 +31,24 @@ function App() {
   }
 
   return (
-    <div className="min-h-screen w-full bg-gray-100 p-8 
-            flex flex-col space-y-8 
-            md:flex-row md:space-x-8 md:space-y-0 
-            justify-center items-start">
+    <div className="min-h-screen w-full bg-gray-100  
+            flex flex-col gap-10  
+            justify-center items-center pt-2 ">
+
+      <Timer initialSeconds={timerLength} onExpire={() => { }} />
+      <Card title="Pictionary" items={cardItems} />
+      <button
+        onClick={generateRandomCard}
+        className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      >
+        Shuffle
+      </button>
       <Sidebar
         timerLength={timerLength}
         onTimerLengthChange={setTimerLength}
         wordSources={allowedOptions}
         onWordSourcesChange={setAllowedOptions}
       />
-      <div className="flex flex-col gap-10">
-        <Timer initialSeconds={timerLength} onExpire={() => { }} />
-        <Card title="Pictionary" items={cardItems} />
-        <button
-          onClick={generateRandomCard}
-          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-        >
-          Shuffle
-        </button>
-      </div>
     </div>
   )
 }
